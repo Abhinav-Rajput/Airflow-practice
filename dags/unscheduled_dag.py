8a525e541a1ed7a70818b94e72318284b397e671
Abhinav-Rajput <abhinavrajput2011@gmail.com>
2021-07-20 00:20:19 +0000

    unscheduled_dag.py

---
+++ dags/unscheduled_dag.py
@@ -0,0 +1,12 @@
+import datetime as dt
+from pathlib import Path
+
+import pandas as pd
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id="01_unscheduled",   # this value would appear on 
+    s
+)
